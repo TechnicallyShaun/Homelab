5015de2706f5b382d64a4256803e8477f479592b
Shaun Smith <shaun@softwaresmith.co.uk>
2024-01-15 22:49:44 +0000

    broken one

---
+++ InstallDotNet.sh
@@ -0,0 +1,7 @@
+# Install Dot Net 8
+
+sudo wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg
+
+sudo gedit /etc/apt/sources.list.d/microsoft.sources
+
+## Need to update a file with source...
